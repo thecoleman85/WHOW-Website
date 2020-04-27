45b65bb2b0f27170dc6c1d5d17e3a67e415d7f81
JohnH21 <60528304+JohnH21@users.noreply.github.com>
2020-04-27 00:23:50 -0500

    Changes
    Most recent update

---
+++ js/transcript.js
@@ -0,0 +1,9 @@
+/* toggle button for transcript */
+function script() {
+  var x = document.getElementById("podScript");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
